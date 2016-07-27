82bc46fab1dedd9337ff82f039ac7dceb0f08141
Jas <jjl_28@hotmail.com>
2016-07-27 12:30:02 -0400

    first python program
    first upload

---
+++ jasminefirstpyhonprogram.py
@@ -0,0 +1,10 @@
+# Jasminefirstprogram.py
+name = input("what is your name?\n")
+print("Hi, ", name)
+
+adjective = input("Please enter an adjective: ")
+noun = input("Please enter a noun: ")
+verb = input("Please enter a verb ending in -ed: ")
+print("your madlib - here is your new sentence")
+print("The", adjective, noun, verb, "over the lazy brown dog.")
+      
